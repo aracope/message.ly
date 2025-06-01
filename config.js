73c1562274cc5dfb1e517d5baf54691fcf36ba64
aracope <156398622+aracope@users.noreply.github.com>
2025-06-01 14:18:30 -0700

    feat: Implement user model methods.

--- config.js
+++ config.js
@@ -5,8 +5,8 @@
 require("dotenv").config();
 
 const DB_URI = (process.env.NODE_ENV === "test")
-  ? "postgresql:///messagely_test"
-  : "postgresql:///messagely";
+  ? process.env.DATABASE_URL_TEST || "postgresql:///messagely_test"
+  : process.env.DATABASE_URL || "postgresql:///messagely";
 
 const SECRET_KEY = process.env.SECRET_KEY || "secret";
 
--- db.js
+++ db.js
@@ -6,7 +6,12 @@ const { DB_URI } = require("./config");
 
 const client = new Client(DB_URI);
 
-client.connect();
+client.connect()
+    .then(() => console.log("Connected to DB"))
+    .catch(err => {
+        console.error("Connection to DB failed, error:", err.stack);
+        process.exit(1);
+    });
 
 
 module.exports = client;
